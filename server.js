9db9ecff90a0206d9d063e6e50e4931d26106a07
SinghSUBHLEEN <singhcherry9563@gmail.com>
2024-01-22 23:30:52 +0530

    deployBuild2

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static(__dirname + "/client/dist"));
+app.use(express.static(__dirname + "/Client/dist"));
 
 app.all("*", (req, res) => {
-    res.status(201).sendFile(__dirname + "/client/dist/index.html");
+    res.status(201).sendFile(__dirname + "/Client/dist/index.html");
 });
 
 app.listen(5000, console.log("Listening at 5000"));
